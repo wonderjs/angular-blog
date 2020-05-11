ad0781017e6d42c98ba61294598725bb8c692897
Maria Filippova <maria.filippova@sibers.com>
2020-05-11 21:53:39 +0700

    Add auth guard

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -10,14 +10,15 @@ import { CreatePageComponent } from './create-page/create-page.component';
 import { EditPageComponent } from './edit-page/edit-page.component';
 import {AuthService} from './shared/services/auth.service';
 import {SharedModule} from '../shared/shared.module';
+import {AuthGuard} from './shared/services/auth.guard';
 
 const routes = [
   {path: '', component: AdminLayoutComponent, children: [
     {path: '', redirectTo: '/admin/login', pathMatch: 'full'},
     {path: 'login', component: LoginPageComponent},
-    {path: 'dashboard', component: DashboardPageComponent},
-    {path: 'create', component: CreatePageComponent},
-    {path: 'post/:id/edit', component: EditPageComponent}
+    {path: 'dashboard', component: DashboardPageComponent, canActivate: [AuthGuard]},
+    {path: 'create', component: CreatePageComponent, canActivate: [AuthGuard]},
+    {path: 'post/:id/edit', component: EditPageComponent, canActivate: [AuthGuard]}
     ]
   }
 ]
@@ -41,7 +42,8 @@ const routes = [
     RouterModule,
   ],
   providers: [
-    AuthService
+    AuthService,
+    AuthGuard
   ]
 })
 export class AdminModule {

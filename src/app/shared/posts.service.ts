3b6a06c9166d27151649eed63f8d4c2e9f0b9be8
Maria Filippova <maria.filippova@sibers.com>
2020-05-27 13:25:05 +0700

    Add post edit page

--- src/app/shared/posts.service.ts
+++ src/app/shared/posts.service.ts
@@ -35,7 +35,22 @@ export class PostsService {
       }))
   }
 
+  getById(id: string): Observable<Post> {
+    return this.http.get<Post>(`${environment.fbDbUrl}/posts/${id}.json`)
+      .pipe(map((post: Post) => {
+        return {
+          ...post,
+          id,
+          date: new Date(post.date)
+        }
+      }))
+  }
+
   remove(id: string): Observable<void> {
     return this.http.delete<void>(`${environment.fbDbUrl}/posts/${id}.json`)
+  }
+
+  update(post: Post): Observable<Post> {
+    return this.http.patch<Post>(`${environment.fbDbUrl}/posts/${post.id}.json`, post)
   }
 }

b51e71a3079472e9e1919289c451fcbff6654eeb
Maria Filippova <maria.filippova@sibers.com>
2020-05-27 12:48:28 +0700

    Update dashboard page

--- src/app/shared/posts.service.ts
+++ src/app/shared/posts.service.ts
@@ -21,4 +21,21 @@ export class PostsService {
         }
       }))
   }
+
+  getAll(): Observable<Post[]> {
+    return this.http.get(`${environment.fbDbUrl}/posts.json`)
+      .pipe(map((response: {[key: string]: any}) => {
+        return Object
+          .keys(response)
+          .map(key => ({
+            ...response[key],
+            id: key,
+            date: new Date(response[key].date)
+          }))
+      }))
+  }
+
+  remove(id: string): Observable<void> {
+    return this.http.delete<void>(`${environment.fbDbUrl}/posts/${id}.json`)
+  }
 }
